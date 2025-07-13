7d64e5f6fd9fd18a5129da24357c6c72e727a49a
Dmitry Timirov <da.timirov@yandex.ru>
2025-07-13 17:56:18 +0500

    correct format

--- src/widget.py
+++ src/widget.py
@@ -1,11 +1,12 @@
-from src.masks import get_mask_account, get_mask_card_number
 from datetime import datetime
 
+from src.masks import get_mask_account, get_mask_card_number
 
-def processing_inform_card_account(card_account_information):
+
+def processing_inform_card_account(card_account_information: str) -> str:
     """Функция принимает номер карты или счета и возвращает замаскированный номер"""
     list_information = card_account_information.split()
-    pay_list= ["счет", "visa", "mastercard", "discover", "maestro", "UnionPay", "JCB", "jcb", "мир"]
+    pay_list = ["счет", "visa", "mastercard", "discover", "maestro", "UnionPay", "JCB", "jcb", "мир"]
 
     a = list_information[0].lower() in pay_list
     b = list_information[-1].isdigit()
@@ -21,7 +22,8 @@ def processing_inform_card_account(card_account_information):
                 mask_number = str(list_information[0]) + " " + get_mask_card_number(list_information[-1])
                 return mask_number
             else:
-                mask_number = str(list_information[0]) + " " + str(list_information[1]) + " " + get_mask_card_number(list_information[-1])
+                mask_number = str(list_information[0]) + " " + str(list_information[1]) + " " + \
+                    get_mask_card_number(list_information[-1])
                 return mask_number
     else:
         return "It's not corrected input"
@@ -31,10 +33,6 @@ def get_date(date_str: str) -> str:
     """Функция возвращает строку в формате даты"""
     full_data = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
     return full_data.strftime("%d.%m.%Y")
-
-
-
-
 
 
 print(processing_inform_card_account("Visa Gold 5999414228426353"))
