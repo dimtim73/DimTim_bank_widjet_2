32082c6a777f9a01664a02f88ec14f4fbd353b79
dimtim73 <106393750+dimtim73@users.noreply.github.com>
2025-08-14 00:33:08 +0500

    update all project files (#2)
    add tests
    
    Co-authored-by: Dmitry Timirov <da.timirov@yandex.ru>

---
+++ tests/test_masks.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.masks import  get_mask_card_number, get_mask_account
+
+# Использование фикстур
+@pytest.fixture
+def all_number():
+    return ['1234567812345678', '1234567812345679']
+
+
+def test_get_mask_card_number(all_number):
+    expected_results = ['1234 56** **** 5678', "1234 56** **** 5679"]  # Ожидаемые результаты для каждого элемента
+    for number, expected in zip(all_number, expected_results):
+        assert get_mask_card_number(number) == expected
+
+
+def test_get_mask_account(all_number):
+    expected_results = ['1234 56** **** 5678', "1234 56** **** 5679"]  # Ожидаемые результаты для каждого элемента
+    for number, expected in zip(all_number, expected_results):
+        assert get_mask_account(number) == expected
+
