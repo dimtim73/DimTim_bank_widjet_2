a515b3539159e8153f308437c2ebb6d668d040e2
Dmitry Timirov <da.timirov@yandex.ru>
2025-08-16 22:31:46 +0500

    update all project files add tests 250816

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
-def filter_by_state(list_vocab: list, state = 'EXECUTED') -> list:
-    """Функция возвращает строку с замаскированным номером. Для карт и счетов используются разные типы маскировки."""
+def filter_by_state(list_vocab: list, state: str = 'EXECUTED') -> list:
+    """Функция возвращает список словарей, отфильтрованных по ключу 'state'."""
     filtered_list = []
     for vocab in list_vocab:
         if vocab['state'] == state:
@@ -7,14 +7,18 @@ def filter_by_state(list_vocab: list, state = 'EXECUTED') -> list:
     return filtered_list
 
 
-def sort_by_date(list_vocab_filter: list, sort_way = True) -> list:
+def sort_by_date(list_vocab_filter: list, sort_way: bool=True) -> list:
     """Функция возвращает новый список словарей, отсортированный по дате (date)."""
     try:
         list_vocab_sort = sorted(list_vocab_filter, key=lambda x: x['date'], reverse=sort_way)
     except Exception:
-        return "It's not corrected input"
+        temp_list = ["It's not corrected input"]
+        return temp_list
     else:
         return list_vocab_sort
 
 # print(filter_by_state([{'id': 939719570, 'state': 'EXECTED', 'date': '2018-06-30T02:08:58.425572'}]))
-print(sort_by_date(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])))
+# print(sort_by_date(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#                                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#                                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#                                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])))
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -4,19 +4,21 @@ from src.widget import processing_inform_card_account, get_date
 
 # Использование параметризации
 
+
 @pytest.mark.parametrize("x, expected", [("Visa Gold 1234", "It's not corrected input"),
                                          ("Visa Gold 123456781234567812", "It's not corrected input"),
                                          ("Visa Gold 1234567812345678", "Visa Gold 1234 56** **** 5678"),
-                                         ("Счет 123451234512345","It's not corrected input"),
-                                         ("Счет 123451234512345123456","It's not corrected input"),
-                                         ("Счет 12345123451234512345","Счет **2345"),
+                                         ("Счет 123451234512345", "It's not corrected input"),
+                                         ("Счет 123451234512345123456", "It's not corrected input"),
+                                         ("Счет 12345123451234512345", "Счет **2345"),
                                          ("", "It's not corrected input")])
-def test_processing_inform_card_account(x, expected):
+def test_processing_inform_card_account(x: str, expected: str) -> None:
     assert processing_inform_card_account(x) == expected
+
 
 @pytest.mark.parametrize("y, expected", [("2024-03-11T02:26:18.671407", "11.03.2024"),
                                          ("2024-03-11T02:26:18.6714071", "It's not corrected input"),
                                          ("2024-03-T02:26:18.6714071", "It's not corrected input"),
-                                         ("","It's not corrected input")])
-def test_get_date(y, expected):
+                                         ("", "It's not corrected input")])
+def test_get_date(y: str, expected: str) -> None:
     assert get_date(y) == expected
